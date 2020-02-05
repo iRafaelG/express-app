885076d88eed308836764353f207e08bbede9e9e
rafaelgilete <rafaelgiletemogena@outlook.com>
2020-02-06 00:07:38 +0100

    Correcciones menores

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 /* Imports
  * Importamos el modulo Express y los almacenamos en una constante llamada "express"
  * Al ejecutar Express, nos devuelve un objeto que es el propio servidor, y lo almacenamos en una constante llamada "app"
- * Importamos en middleware Morgan y los almacenamos en una constante llamada "morgan"
+ * Importamos el middleware Morgan y los almacenamos en una constante llamada "morgan"
  */
 
 const express = require("express");
@@ -28,18 +28,18 @@ app.set("view engine", "ejs");
 
 /* Middlewares
  * Indicamos al servidor que middlewares debe usar, en este caso JSON y Morgan
- * JSON viene en incorporado con Express, asi que, no debemos importar ningun modulo adicional, solo usarlo
+ * JSON viene incorporado con Express, asi que, no debemos importar ningun modulo adicional, solo usarlo
  * A Morgan debemos de indicarle un modo de operar, es este caso "dev"
  * "logger" seria la funcion middleware que hemos creado anteriormente que se "parece" a Morgan
  */
 
 app.use(express.json());
-app.use(morgan("combined"));
+app.use(morgan("dev"));
 // app.use(logger);
 
 /* Routes
  * Usando los verbos del protocolo HTTP podemos definir como funcionan las rutas
- * A traves de un URI ("/", "/user"...) y una funcion (handle) que las maneje
+ * a traves de una URI ("/", "/user"...) y una funcion (handler) que las maneje
  * Mediante el verbo GET, le indicamos con que funcion debe manejar la ruta inicial por ejemplo
  * La propiedad "all" nos permite indicar que sucede para todas las rutas que tengan la misma URI
 
@@ -52,7 +52,6 @@ app.all('/user', (req, res, next) => {
 
 app.get("/", (req, res) => {
   let data = [
-    { name: "Vero" },
     { name: "Rafael" },
     { name: "Javier" },
     { name: "Miguel" }
